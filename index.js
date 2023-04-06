4a79fea6b988ef0d431ec0a30209f49bf0c7b7a7
dineshsai463 <dineshsai463@example.com>
2023-04-06 12:44:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const bodyParser = require('body-parser');
+const express=require('express')
+const dbConnect = require('./config/dbconnect')
+const app=express()
+const dotenv=require('dotenv').config();
+const PORT=process.env.PORT || 4000;
+const authRouter=require('./routes/authRoute')
+const productRouter=require('./routes/productRoute')
+const blogRouter=require('./routes/blogRoute')
+const categoryRouter=require('./routes/prodcategoryRoute')
+const blogcatRouter=require('./routes/blogCatRoute')
+const brandRouter=require('./routes/brandRoute')
+const colorRouter=require('./routes/colorRoute')
+const enqRouter=require('./routes/enqRoute')
+const couponRouter=require('./routes/couponRoute')
+const uploadRouter=require('./routes/uploadRoute')
+const cors = require("cors");
+const { errorHandler, notFound } = require('./middlewares/errorHandler');
+const cookieParser= require('cookie-parser');
+const morgan=require('morgan')
+
+dbConnect();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/user", authRouter);
+app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/blogcategory", blogcatRouter);
+app.use("/api/brand", brandRouter);
+app.use("/api/color", colorRouter);
+app.use("/api/enquiry", enqRouter);
+app.use("/api/coupon", couponRouter);
+app.use("/api/upload", uploadRouter);
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running  at PORT ${PORT}`);
+  });
+  
---
+++ routes/productRoute.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, AddtoWishlist, rating } = require('../controller/productCtrl');
+const {isAdmin,authMiddleware} =require('../middlewares/authMiddleware');
+const router=express.Router();
+
+
+router.post('/',authMiddleware,createProduct,isAdmin)
+router.get('/:id',getaProduct)
+router.put('/wishlist',authMiddleware,AddtoWishlist)
+router.put('/rating',authMiddleware,rating)
+router.put('/:id',authMiddleware,updateProduct,isAdmin)
+router.delete('/:id',authMiddleware,deleteProduct,isAdmin)
+router.get('/',getAllProduct)
+
+module.exports=router
